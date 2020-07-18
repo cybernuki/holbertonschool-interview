23de5a66ad531477986537adca66673ad5c172e4
hi <11asdas>
2020-07-18 15:38:58 -0500

    solve the lockboxes problem

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    hasOpen = {0: 1}
+
+    if not boxes:
+        return False
+
+    openBox(boxes[0], boxes, hasOpen)
+    return True if (len(hasOpen) == len(boxes)) else False
+
+
+def openBox(keys, boxes, hasOpen):
+    if not keys:
+        return {}
+    for key in keys:
+        if not hasOpen.get(key):
+            hasOpen.update({key: 1})
+            hasOpen.update(openBox(boxes[key], boxes, hasOpen))
+    return hasOpen
