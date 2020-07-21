4c54ff72117cc4d3b2ae6d678a47a0c36b3ad1b8
hi <11asdas>
2020-07-20 20:52:05 -0500

    include keys less than zero and greater than the boxes' lenght

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -14,7 +14,7 @@ def openBox(keys, boxes, hasOpen):
     if not keys:
         return {}
     for key in keys:
-        if key >= 0 and key < len(hasOpen) and not hasOpen.get(key):
+        if key >= 0 and key < len(boxes) and not hasOpen.get(key):
             hasOpen.update({key: 1})
             hasOpen.update(openBox(boxes[key], boxes, hasOpen))
     return hasOpen
