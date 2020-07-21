16169c481cf58d0181c3a5db4f28d3ba98a1189e
hi <11asdas>
2020-07-21 12:47:54 -0500

    try with iterative way

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,20 +2,18 @@
 
 
 def canUnlockAll(boxes):
-    hasOpen = {0: 1}
-
     if not boxes:
         return False
 
-    openBox(boxes[0], boxes, hasOpen)
-    return True if (len(hasOpen) == len(boxes)) else False
+    opened = {}
+    queue = [0]
 
+    while queue:
+        boxNum = queue.pop(0)
+        opened[boxNum] = 1
+        for key in boxes[boxNum]:
+            if key >= 0 and key < len(boxes) and not opened.get(key)\
+                    and (key not in queue):
+                queue.append(key)
 
-def openBox(keys, boxes, hasOpen):
-    if not keys:
-        return {}
-    for key in keys:
-        if key >= 0 and key < len(boxes) and not hasOpen.get(key):
-            hasOpen.update({key: 1})
-            hasOpen.update(openBox(boxes[key], boxes, hasOpen))
-    return hasOpen
+    return True if (len(opened) == len(boxes)) else False
