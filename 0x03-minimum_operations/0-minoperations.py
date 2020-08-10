87a9d60e88128b2c564428d12a930c3b11c14d32
hi <11asdas>
2020-08-10 10:33:53 -0500

    use another waY

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """Minimum Operations
-In a text file, there is a single character H. Your text editor can execute
-only two operations in this file: Copy All and Paste. Given a number n, write
-a method that calculates the fewest number of operations needed to result in
-exactly n H characters in the file.
 """
 
 
 def minOperations(n):
-    """Find the least amount of operations to get to a specific amount of Hs"""
+    """minOperations"""
     counter = 1
     pastebin = 0
     operations = 0
