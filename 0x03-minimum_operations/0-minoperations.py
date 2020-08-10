6288a6e7287575f42e642dae4239557dcd514f16
hi <11asdas>
2020-08-10 10:31:07 -0500

    em

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,48 +1,24 @@
 #!/usr/bin/python3
-from math import sqrt
-
-"""
-    Get the minumun operation iterative
-    it no take a lot time
+"""Minimum Operations
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file: Copy All and Paste. Given a number n, write
+a method that calculates the fewest number of operations needed to result in
+exactly n H characters in the file.
 """
 
 
 def minOperations(n):
-    """min operations"""
-    if n <= 1:
+    """Find the least amount of operations to get to a specific amount of Hs"""
+    counter = 1
+    pastebin = 0
+    operations = 0
+
+    if n <= 0:
         return 0
-
-    if (isPrime(n)):
-        return n
-    else:
-        return sumPrimeFactors(n)
-
-
-def isPrime(n):
-    """Is prime"""
-    for i in range(2, int(sqrt(n)) + 1):
-        if n % i == 0:
-            return 0
-    return 1
-
-
-def sumPrimeFactors(n):
-    """sumPrimeFactor"""
-    sum = 0
-    i = 2
-
-    while n % i == 0:
-        n //= i
-        sum += i
-    i = 3
-    while n > 1:
-        if n % i == 0:
-            n //= i
-            sum += i
-        else:
-            i += 2
-        if n > 1 and isPrime(n):
-            sum += n
-            break
-
-    return sum
+    while counter != n:
+        if n % counter == 0:
+            pastebin = counter
+            operations += 1
+        counter = counter + pastebin
+        operations += 1
+    return operations
