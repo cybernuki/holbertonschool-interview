127f7988e137af47567fe0c95e8d78c2cc8a9a19
--replace-all <1164@holbertonschool.com>
2020-09-03 06:54:59 +0000

    improve space complexity

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,43 +1,6 @@
 #include "lists.h"
 
 /**
- * put - put a new node in the stack
- * @stack: is the stack list
- * @origin: is the origin node to add
- */
-void put(listint_t **stack, listint_t *origin)
-{
-	listint_t *newNode = NULL;
-
-	if (!stack)
-		return;
-
-	newNode = malloc(sizeof(listint_t *));
-	if (!newNode)
-		exit(1);
-
-	newNode->n = origin->n;
-	newNode->next = (*stack);
-	*stack = newNode;
-}
-
-/**
- * freeStack - free a stack
- * @stack: is the stack
- */
-void freeStack(listint_t *stack)
-{
-	listint_t *next = NULL, *index = stack;
-
-	while (index)
-	{
-		next = index->next;
-		free(index);
-		index = next;
-	}
-}
-
-/**
  * is_palindrome - determines if a linked list is a palindrome
  * @head: is the head of the linkedlist
  * Return: 1 if it is a palindrome, 0 in otherwise
@@ -45,37 +8,25 @@ void freeStack(listint_t *stack)
 int is_palindrome(listint_t **head)
 {
 	short isPalindrome = 1;
-	listint_t *index = *head, *stack = NULL, *stackIndex = NULL;
-	size_t size = 0, mid = 0, i = 0;
+	int arr[2048];
+	int h = 0, l = 0;
+	listint_t *index = NULL;
 
-	if (!head || !(*head))
+	if (!head || !(*head) || !(*head)->next)
 		return (isPalindrome);
 
 	while (index)
 	{
+		arr[l++] = index->n;
 		index = index->next;
-		size++;
 	}
 
-	mid = size / 2, index = *head;
-	while (index)
+	while (h < l && isPalindrome)
 	{
-		if (i >= mid)
-			put(&stack, index);
-		index = index->next;
-		i++;
-	}
-	i = 0, index = *head, stackIndex = stack;
-	while (i < mid && isPalindrome)
-	{
-		if (index->n != stackIndex->n)
+		if (arr[h] != arr[l])
 			isPalindrome = 0;
-
-		index = index->next;
-		stackIndex = stackIndex->next;
-		i++;
+		h++, l--;
 	}
 
-	freeStack(stack);
 	return (isPalindrome);
 }
