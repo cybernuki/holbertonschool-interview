23d524ad4b29e7f4b7268d11e4cf65097a6f3c18
--replace-all <1164@holbertonschool.com>
2020-09-11 02:23:07 +0000

    Add Challenge

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - determines if a given number is palindrome
+ * @n: the given number
+ * Return: 1 if it is a palindrome, 0 in otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	int arr[50], h = 0, l = 0;
+	unsigned long power = 1;
+
+	while ((n / power) >= 1)
+	{
+		arr[h] = (n / power) % 10;
+		power *= 10, h++;
+	}
+
+	h--;
+	while (h > l)
+	{
+		if (arr[l] != arr[h])
+			return (0);
+		l++, h--;
+	}
+
+	return (1);
+}
