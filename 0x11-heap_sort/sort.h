15b64daf921d6d66b790eda799833fae71f95c55
cybernuki <1164@holbertonschool.com>
2020-12-06 22:28:58 +0000

    otro

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,10 +1,11 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_array(const int *array, size_t size);
+void swap(int *a, int *b, int *array, size_t n);
+void heap_root(int *arr, int n, int i, size_t size);
 void heap_sort(int *array, size_t size);
-
-#endif
+#endif /* _SORT_H_ */
