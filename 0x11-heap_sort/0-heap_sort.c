70fd4baf956597c5360a27ec2fcb82a193dda43c
cybernuki <1164@holbertonschool.com>
2020-12-06 22:37:48 +0000

    fix

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -11,7 +11,7 @@ void heapify(int *arr, int n, int i)
 	int tmp = 0;
 	int largest = i;
 	int left = 2 * i + 1;
-	int right = left + 1;
+	int right = 2 * i + 2;
 
 	if (left < n && arr[left] > arr[largest])
 		largest = left;
